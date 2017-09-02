789eeb79d6ac4ae4299df3eee75d806b1bfff4e8
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-09-02 15:43:58 -0600

    It works!

---
+++ script.js
@@ -0,0 +1,388 @@
+// Tic-Tac-Toe - Script.js - Javascript - March 8, 2014
+
+// **** Global Variable Declarations ****
+
+var nBoardDimension = 3;
+var nBoardWidth = nBoardDimension;
+var nBoardHeight = nBoardDimension;
+var nBoardArea = nBoardWidth * nBoardHeight;
+var boardPopulation;
+var aBoardImageNumbers = null;  // new Array(nBoardArea);
+var EmptyNumber = -1;
+var victoryValue = 100;
+var defeatValue = -victoryValue;
+
+var XNumber = 0;
+var ONumber = 1;
+var NumberOfCurrentPlayer;
+var boardPopulation;
+var PlayerNames = ["X", "O"];
+// var PlayerIsAutomated = [false, false];
+var PlayerIsAutomated = [false, true];
+var PlayerPly = [6, 6];
+var isGameOver;
+var isXVictory;
+var isOVictory;
+
+// **** jQuery Function Declarations ****
+
+$(document).ready(function () {
+    onPageLoad();
+});
+
+// **** Function Declarations ****
+
+function isVictory(player, row, column)
+{
+    // 1) Check the specified row.
+    var victory = true;
+
+    for (var column2 = 0; column2 < nBoardDimension; ++column2)
+    {
+
+        if (aBoardImageNumbers[row * nBoardDimension + column2] != player)
+        {
+            victory = false;
+            break;
+        }
+    }
+
+    if (victory)
+    {
+        return true;
+    }
+
+    // 2) Check the specified column.
+    victory = true;
+
+    for (var row2 = 0; row2 < nBoardDimension; ++row2)
+    {
+
+        if (aBoardImageNumbers[row2 * nBoardDimension + column] != player)
+        {
+            victory = false;
+            break;
+        }
+    }
+
+    if (victory)
+    {
+        return true;
+    }
+
+    if (row == column)
+    {
+        // 3) Check the primary diagonal.
+        victory = true;
+
+        for (var i = 0; i < nBoardDimension; ++i)
+        {
+
+            if (aBoardImageNumbers[i * nBoardDimension + i] != player)
+            {
+                victory = false;
+                break;
+            }
+        }
+
+        if (victory)
+        {
+            return true;
+        }
+    }
+
+    if (row + column == nBoardDimension - 1)
+    {
+        // 4) Check the secondary diagonal.
+        victory = true;
+
+        for (var i = 0; i < nBoardDimension; ++i)
+        {
+
+            if (aBoardImageNumbers[i * nBoardDimension + nBoardDimension - 1 - i] != player)
+            {
+                victory = false;
+                break;
+            }
+        }
+
+        if (victory)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function placePiece(player, row, column, displayMove) // displayMove is not used.
+{
+    // If player is X or O, the square being written to must be empty just before the move is made.
+    // If player is Empty, the square being written to must be non-empty just before the move is made, and displayMove must be false.
+
+    if (row < 0 || row >= nBoardDimension)
+    {
+        alert("PlacePiece() : row " + row + " is out of range; nBoardDimension == " + nBoardDimension);
+        return false;
+    }
+
+    if (column < 0 || column >= nBoardDimension)
+    {
+        alert("PlacePiece() : column is out of range.");
+        return false;
+    }
+
+    var oldSquareContent = aBoardImageNumbers[row * nBoardDimension + column];
+
+    if (player != EmptyNumber)
+    {
+
+        if (oldSquareContent != EmptyNumber)
+        {
+            alert("PlacePiece() : Attempted to write an X or an O into a non-empty square.");
+            return false;
+        }
+    }
+    else
+    {
+
+        if (oldSquareContent == EmptyNumber)
+        {
+            alert("PlacePiece() : Attempted to erase an already-empty square.");
+            return false;
+        }
+    }
+
+    aBoardImageNumbers[row * nBoardDimension + column] = player;
+
+    if (player == EmptyNumber)
+    {
+        --boardPopulation;
+    }
+    else
+    {
+        ++boardPopulation;
+    }
+
+    var victory = player != EmptyNumber && isVictory(player, row, column);
+
+    return victory; // This can return true for real or speculative moves.
+}
+
+// ****
+
+function getImagePath(imageNumber) {
+    var imageName = "empty";
+
+    if (imageNumber == XNumber) {
+        imageName = "x";
+    } else if (imageNumber == ONumber) {
+        imageName = "o";
+    }
+
+    return "images/" + imageName + ".png";
+}
+
+function displayTurnMessage() {
+    var turnMessage;
+
+    if (!isGameOver) {
+        turnMessage = PlayerNames[NumberOfCurrentPlayer];
+
+        if (PlayerIsAutomated[NumberOfCurrentPlayer]) {
+            turnMessage = turnMessage + " is thinking...";
+        } else {
+            turnMessage = turnMessage + "'s turn.";
+        }
+    } else {
+
+        if (isXVictory) {
+            turnMessage = PlayerNames[XNumber] + " wins.";
+        } else if (isOVictory) {
+            turnMessage = PlayerNames[ONumber] + " wins.";
+        } else {
+            turnMessage = "Tie game.";
+        }
+
+        turnMessage = "Game over; " + turnMessage;
+    }
+
+    $("#turnMessage").html(turnMessage);
+}
+
+function moveHelper(row, col) {
+    var isVictory = placePiece(NumberOfCurrentPlayer, row, col, true);
+
+    $("[name='squares']").eq(row * nBoardDimension + col).prop("src", getImagePath(NumberOfCurrentPlayer));
+
+    isGameOver = isVictory || boardPopulation == nBoardArea;
+
+    if (isVictory) {
+
+        if (NumberOfCurrentPlayer == XNumber) {
+            isXVictory = true;
+        } else {
+            isOVictory = true;
+        }
+    }
+
+    NumberOfCurrentPlayer = 1 - NumberOfCurrentPlayer;
+    displayTurnMessage();
+
+    if (!isGameOver && PlayerIsAutomated[NumberOfCurrentPlayer]) {
+        setTimeout("automatedMove()", 100);     // Wait for 100 ms before the next move to give the browser time to update the board.
+    }
+}
+
+function getJSONTicTacToeRequest (boardString, maxPly, descriptor = {}) {
+	let webServerProtocol = descriptor.protocol || 'http';
+	let webServerName = descriptor.name || 'localhost';
+	let webServerPort = descriptor.port || 3000;
+	// let url = webServerProtocol + '://' + webServerName + ':' + webServerPort + '/tictactoe/' + boardString.replace(/ /g, 'E') + '/' + maxPly;
+	// let url = webServerProtocol + '://' + webServerName + ':' + webServerPort + '/tictactoe/' + boardString + '/' + maxPly;
+	let url = '/foo/' + boardString + '/' + maxPly;
+
+	// This is essentially an augmented version of jQuery's AJAX $.getJSON()
+	// See https://api.jquery.com/jquery.getjson/
+	$.ajax({								// eslint-disable-line no-undef
+		dataType: 'json',
+		url: url,
+		success: function (result) {
+			const message = 'getJSONRequest() sent to \'' + url + '\' succeeded; result is:';
+
+			console.log(message, result);
+			// alert(message + ' ' + JSON.stringify(result));	// eslint-disable-line no-alert
+			moveHelper(result.bestRow, result.bestColumn);
+		},
+		error: function (error) {
+			const message = 'getJSONTicTacToeRequest() sent to \'' + url + '\' failed; error is: ' + error.status + ' ' + error.statusText;
+
+			console.error(message);
+			alert(message);									// eslint-disable-line no-alert
+		}
+	});
+}
+
+function automatedMove() {
+	// var returnObject = findBestMove(NumberOfCurrentPlayer, PlayerPly[NumberOfCurrentPlayer], defeatValue - 1, true);
+
+	// moveHelper(returnObject.bestRow, returnObject.bestCol);
+
+	let boardString = aBoardImageNumbers.map(n => {
+		
+		if (n === 0) {
+			return 'X';
+		} else if (n === 1) {
+			return 'O';
+		} else {
+			return 'E';
+		}
+	}).join('');
+
+	getJSONTicTacToeRequest(boardString, PlayerPly[NumberOfCurrentPlayer]);
+}
+
+function squareClicked(i) {
+
+    if (isGameOver || PlayerIsAutomated[NumberOfCurrentPlayer]) {
+        return;
+    }
+
+    var row = parseInt(i / nBoardWidth, 10);
+    var col = i % nBoardWidth;
+
+    moveHelper(row, col);
+}
+
+function populateLookaheadDDL(ddlID) {
+    $("#" + ddlID).html("");    // Clear the list.
+
+    for (var i = 1; i <= 9; ++i) {
+        $("<option>" + i + "</option>").appendTo("#" + ddlID); // Perhaps we could reverse this and use append instead of appendTo.
+    }
+}
+
+function constructBoard() {
+    var pathToEmptyImage = getImagePath(EmptyNumber);
+    var i = 0;
+
+    for (var r = 0; r < nBoardHeight; ++r) {
+        var rowName = "row" + r;
+
+        $("<tr id='" + rowName + "'></tr>").appendTo("#board");
+
+        for (var c = 0; c < nBoardWidth; ++c) {
+            $("<td><img name='squares' class='tightBox' src='" + pathToEmptyImage + "' onclick='squareClicked(" + i + ")' /></td>").appendTo("#" + rowName);
+            ++i;
+        }
+    }
+
+    populateLookaheadDDL("ddlLookaheadX");
+    populateLookaheadDDL("ddlLookaheadO");
+
+    $("#cbAutomateX").prop("checked", PlayerIsAutomated[XNumber]);
+    $("#cbAutomateO").prop("checked", PlayerIsAutomated[ONumber]);
+    $("#ddlLookaheadX").val(PlayerPly[XNumber]);
+    $("#ddlLookaheadO").val(PlayerPly[ONumber]);
+    $("#ddlLookaheadX").prop("disabled", !PlayerIsAutomated[XNumber]);
+    $("#ddlLookaheadO").prop("disabled", !PlayerIsAutomated[ONumber]);
+    aBoardImageNumbers = new Array(nBoardArea);
+    newGame();
+}
+
+// **** Event Handlers ****
+
+function onPageLoad() {
+    constructBoard();
+}
+
+function newGame() {
+    var pathToEmptyImage = getImagePath(EmptyNumber);
+
+    for (var i = 0; i < aBoardImageNumbers.length; ++i) {
+        aBoardImageNumbers[i] = EmptyNumber;
+    }
+
+    $("[name='squares']").each(function () {
+        $(this).prop("src", pathToEmptyImage);
+    });
+
+    NumberOfCurrentPlayer = XNumber;
+    boardPopulation = 0;
+    isGameOver = false;
+    isXVictory = false;
+    isOVictory = false;
+    displayTurnMessage();
+
+    if (PlayerIsAutomated[NumberOfCurrentPlayer]) {
+        setTimeout("automatedMove()", 100);
+    }
+}
+
+function cbAutomateX_onChange() {
+    PlayerIsAutomated[XNumber] = $("#cbAutomateX").prop("checked");
+    $("#ddlLookaheadX").prop("disabled", !PlayerIsAutomated[XNumber]);
+
+    if (!isGameOver && PlayerIsAutomated[XNumber] && NumberOfCurrentPlayer == XNumber) {
+        automatedMove();
+    }
+}
+
+function cbAutomateO_onChange() {
+    PlayerIsAutomated[ONumber] = $("#cbAutomateO").prop("checked");
+    $("#ddlLookaheadO").prop("disabled", !PlayerIsAutomated[ONumber]);
+
+    if (!isGameOver && PlayerIsAutomated[ONumber] && NumberOfCurrentPlayer == ONumber) {
+        automatedMove();
+    }
+}
+
+function ddlLookaheadX_onChange() {
+    PlayerPly[XNumber] = parseInt($("#ddlLookaheadX").val(), 10);
+}
+
+function ddlLookaheadO_onChange() {
+    PlayerPly[ONumber] = parseInt($("#ddlLookaheadO").val(), 10);
+}
+
+// **** End of File ****
---
+++ src/app.js
@@ -0,0 +1,153 @@
+﻿// node-js-express-template/app.js
+
+// An example of a simple Express.js Web server.
+// Tom Weatherhead - August 1, 2017
+
+'use strict';
+
+// require('rootpath')();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// **** Cross-Origin Resource Sharing: Begin ****
+
+// See https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+// See https://enable-cors.org/server_expressjs.html
+
+// General:
+
+// If we uncomment the block below, Mocha will complain that "the header contains invalid characters".
+
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+// });
+
+// Minimal:
+
+// app.use(function(req, res, next) {
+	// res.header("Access-Control-Allow-Origin", "null");
+	// res.header("GET");
+	// next();
+// });
+
+// **** Cross-Origin Resource Sharing: End ****
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// **** Request Event Handlers: Begin ****
+
+app.get('/', function (req, res) {
+	console.log('GET / : Sending the file index.html');
+	res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+
+
+
+
+
+const http = require('http');		// See https://nodejs.org/api/http.html
+	
+app.get('/foo/:board([EXO]{9})/:maxPly([0-9]{1})', function(req, res) {
+	let boardString = req.params.board;
+	let maxPly = req.params.maxPly;
+	let web_service_request_options_object = {
+		host: 'localhost',
+		port: 3000,
+		path: '/tictactoe/' + boardString + '/' + maxPly
+	};
+
+	var request = http.get(web_service_request_options_object, (response) => {
+	// var request = http.json(web_service_request_options_object, (response) => {
+		// console.log('The response from the Web service is:', response);
+
+		const statusCode = response.statusCode;
+		const statusMessage = response.statusMessage;
+		const contentType = response.headers['content-type'];
+
+		console.log('Response: Status code:', statusCode, statusMessage);
+		// console.log('Response: Headers:', response.headers);
+		console.log('Response: Content type:', contentType);
+
+		let error;
+
+		if (statusCode !== 200) {
+			// error = new Error(`HTTP request to financial data service failed.\nHTTP status code: ${statusCode}`);
+			error = new Error('HTTPS request to Web service failed.\nHTTPS status: ' + statusCode + ' ' + statusMessage);
+		// } else if (!/^application\/json/.test(contentType)) {
+			// Google Finance does not respond with application/json.
+			// error = new Error(`Invalid content-type.\nExpected application/json but received ${contentType}`);
+		}
+
+		if (error) {
+			var errorMessage = 'Error in https-json-request.service: ' + error.message;
+
+			console.error(errorMessage);
+			response.resume();				// Consume the response data to free up memory.
+			// deferred.reject(errorMessage);
+			res.status(500).send(errorMessage);
+		}
+
+		// console.log('The response from the Web service is:', response);
+		// console.log('response.body:', response.body);
+		// res.json(response.body);
+		response.setEncoding('utf8');
+
+		let rawData = '';
+
+		response.on('data', (chunk) => { rawData += chunk; });
+
+		response.on('end', () => {
+			try {
+				// console.log('The raw data from the Web service response is:', rawData);
+				var jsonData = JSON.parse(rawData);
+				// console.log('The JSON data parsed from the Web service response is:', parsedData);
+				// deferred.resolve(jsonData);
+				res.json(jsonData);
+			} catch (error) {
+				var errorMessage = 'Error in https-json-request.service while parsing the response as JSON: ' + error.message;
+
+				console.error(errorMessage);
+				// deferred.reject(errorMessage);
+				res.status(500).send(errorMessage);
+			}
+		});
+	}).on('error', (error) => {
+		var errorMessage = 'Inside on(error) : Error in https-json-request.service: ' + error.message;
+
+		// console.error(`Got error: ${error.message}`);
+		console.error(error);
+		console.error(errorMessage);
+		deferred.reject(errorMessage);
+	});
+});
+
+
+
+
+
+
+
+
+// app.get('/jquery.min.js', function (req, res) {
+//	res.redirect('https://code.jquery.com/jquery-3.2.1.min.js');
+//	res.sendFile(path.join(__dirname, 'node_modules', 'jquery', 'dist', 'jquery.min.js'));
+// });
+
+app.get('/script.js', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'script.js'));
+});
+
+app.get('/css/style.css', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public', 'css', 'style.css'));
+});
+
+// **** Request Event Handlers: End ****
+
+module.exports = app;
+
+// End of File.
